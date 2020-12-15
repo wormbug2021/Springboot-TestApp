5a712980c6df7dde1569cbf37f3fc74a1300483f
wormbug2021 <jws5017@naver.com>
2020-12-15 17:04:09 +0900

    환경세팅 완료

---
+++ src/main/java/com/app/testapp/test/TestConntroller.java
@@ -0,0 +1,18 @@
+package com.app.testapp.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestConntroller {
+
+	
+	//http://localhost:8080/test/hello 
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+	
+}
